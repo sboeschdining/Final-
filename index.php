507f295b6a7b7361172a43b28d9c232f1c7f540d
sboeschdining <sboeschdining@gmail.com>
2015-04-17 14:53:14 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+include ("top.php")
+?>
+<article id="main">
+   
+    
+
+</article> 
+
+<img src="mensteam (1).png" alt="mensteam"/>
+<img src="womensteam.png" alt="womensteam"/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include ("footer.php");
+?>
+
+</body> 
+
+
+
+
+
+
+
+
+</html>
+
