073fa5b7b5932bb5bf3867442bd245e69c124dd5
sboeschdining <sboeschdining@users.noreply.github.com>
2015-04-22 18:05:51 -0400

    Create Gingras.php

---
+++ Gingras.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <?php
+    include ("top.php");
+    ?>
+    <body>
+        <p><h1><b><center><font face="Calisto MT">Coach Gingras</center></b></h1></font></p> 
+   <img src="gingras.png" alt="gingras" width="260" height="300"/>
+
+    <p>Joe Gingras enters his 13th season as an cross country and track and field coach at Vermont and his second season 
+        as cross country associate head coach. He has been associated with the University of Vermont cross country and 
+        track and field program for the past 17 years.</p>
+
+    <p>During Gingras' coaching tenure, Vermont has won three America East Conference Championships and a New England Championship.
+        Gingras has coached two New England champions and six America East individual champions in track and field, as well as 21
+        other conference top three finishers over the last three years. Vermont runners have also set 10 school records in the last 
+        four years under Gingras' tutelage.</p>
+
+    <p>Gingras was a four-year letterwinner at Vermont in both cross country and track and field during his collegiate career. He was 
+        team captain and MVP of the cross country team as a senior. His specialty on the track was the 3000 meter steeplechase, but 
+        also included races from 1500 to 5000 meters.</p>
+
+    <p>As a junior Gingras earned America East All-Conference honors after placing fifth in the steeplechase at the 1998 America East 
+        Outdoor Track and Field Championships. His fifth place finish also qualified him for the New England Championships later that year.</p>
+
+    <p>As a senior he earned all-conference honors for the second straight year with a third place finish in the steeplechase and finished 
+        11th out of 24 qualifiers at the New England Championships.</p>
+
+    <p>Gingras graduated from Vermont in 1999 with a bachelor's degree in economics and geography. He was the recipient of the John H. 
+        Converse Award given to the outstanding senior in economics and was a member of the Gamma Theta Upsilon International Honor Society 
+        for geography. Gingras was a six-time member of the Dean's List at UVM and six-time member of the America East Academic Honor Roll.</p>
+
+</body>
