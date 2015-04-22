1cecf5962ba83cff9e868edc8581d0a4d4a11885
sboeschdining <sboeschdining@users.noreply.github.com>
2015-04-22 17:57:34 -0400

    Create Meetthecoaches.php

---
+++ Meetthecoaches.php
@@ -0,0 +1,18 @@
+<?php
+/* php code can go anywhere but we will be starting almost all our documents
+ * with php code There is no code here at the moment */
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<?php
+include ("top.php");
+?>
+<body>
+  
+    <p><h1><b><center><font face="Calisto MT">Meet The Coaches</center></b></h1></font></p> 
+<img style="float:right; margin-right:0px; padding-right:0px;" 
+     src="gingras.png" alt="gingras" width="260" height="300"/>
+<img src="belfield.png" alt="belfield"/>
+
+      
+</body>
