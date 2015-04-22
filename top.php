b3749892ae3b19f00f3a4e011db888fbd7c13b7e
sboeschdining <sboeschdining@users.noreply.github.com>
2015-04-22 17:59:02 -0400

    Create top.php

---
+++ top.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+
+<html lang = "en">
+<head>
+<title>Vermont Cross Country</title>
+<meta charset = "utf-8">
+<meta name = "author" content = "Cross Country">
+<meta name = "description" content = "This page will introduce you to both the men and womens cros country team at the University of Vermont.">
+
+<meta name = "viewport" content = "width=device-width, initial-scale=1">
+
+<!--[if lt IE 9]>
+<script src = "//html5shim.googlecode.com/sin/trunk/html5.js"></script> 
+<![endif]--> 
+
+<link rel="stylesheet" href="stylesheet.css" type="text/css" media="screen"> 
+
+<?php
+// %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^% 
+// 
+// PATH SETUP 
+// 
+?>	 
+
+</head> 
+<!-- ################ body section ######################### --> 
+
+<?php
+print '<body id="' . $path_parts['filename'] . '">';
+
+include "header.php";
+include "nav.php";
+?> 
